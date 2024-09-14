79f8287bfd59cef87f7b02c3cce735968d1252a4
Serkan <s8ersis@gmail.com>
2024-09-14 14:39:43 +0300

    update: private router

--- src/routes/PrivateRouter.jsx
+++ src/routes/PrivateRouter.jsx
@@ -2,29 +2,6 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate, NavLink, Outlet } from 'react-router-dom'
 
-// const PrivateRouter = ({allowedRoles=[]}) => {
-//   const { user } = useSelector(state => state.auth)
-//   // const user = true
-//   if (!user) {
-//     return <Navigate to="/login" replace />;
-//   }
-//   const userRoles = [];
-//   if (user.isAdmin) userRoles.push("admin");
-//   if (user.isStaff) userRoles.push("staff");
-//   // if (user.isActive) userRoles.push("user");
-
-//   if (!user || !user.isActive) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   const hasRequiredRole = allowedRoles.some(role => userRoles.includes(role));
-
-//   if (allowedRoles.length > 0 && !hasRequiredRole) {
-//     return <Navigate to="/unauthorized" replace />;
-//   }
-
-//   return <Outlet />;
-// }
 
 const PrivateRouter = ({ allowedRoles = [] }) => {
   const { user,token } = useSelector(state => state.auth);
